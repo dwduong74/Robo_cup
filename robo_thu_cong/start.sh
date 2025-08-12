edbc2100f44218f1659d797ef1d43be9ea00c28b
dwduong74 <147181637+dwduong74@users.noreply.github.com>
2025-08-12 19:51:28 +0700

    Update start.sh

--- robo_thu_cong/start.sh
+++ robo_thu_cong/start.sh
@@ -9,21 +9,21 @@ fi
 
 # Cửa sổ 1 - micro_ros_agent
 gnome-terminal --title="micro_ros_agent" \
-    -- bash -c "cd ~/Robocup/Robot_thu_cong && \
+    -- bash -c "cd ~/Robo_cup/robo_thu_cong && \
                 source /opt/ros/humble/setup.bash && source install/setup.bash && \
                 export ROS_DOMAIN_ID=0 && \
                 ros2 run micro_ros_agent micro_ros_agent udp4 --port 8888; exec bash"
 
 # Cửa sổ 2 - joystick
 gnome-terminal --title="joystick" \
-    -- bash -c "cd ~/Robocup/Robot_thu_cong && \
+    -- bash -c "cd ~/Robo_cup/robo_thu_cong && \
                 source /opt/ros/humble/setup.bash && source install/setup.bash && \
                 export ROS_DOMAIN_ID=0 && \
                 ros2 run control joystick; exec bash"
 
 # Cửa sổ 3 - sim_fth
 gnome-terminal --title="sim_fth" \
-    -- bash -c "cd ~/Robocup/Robot_thu_cong && \
+    -- bash -c "cd ~/Robo_cup/robo_thu_cong && \
                 source /opt/ros/humble/setup.bash && source install/setup.bash && \
                 export ROS_DOMAIN_ID=0 && \
                 sleep 3 && \
@@ -31,7 +31,7 @@ gnome-terminal --title="sim_fth" \
 
 # Cửa sổ 4 - echo topic /esp_vel (đếm ngược 3s)
 gnome-terminal --title="echo_esp_vel" \
-    -- bash -c "cd ~/Robocup/Robot_thu_cong && \
+    -- bash -c "cd ~/Robo_cup/robo_thu_cong && \
                 source /opt/ros/humble/setup.bash && source install/setup.bash && \
                 export ROS_DOMAIN_ID=0 && \
                 rqt_graph "
